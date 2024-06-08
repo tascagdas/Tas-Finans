eaefa57fc17c5c931c6041dbdfcedee69fdcae1d
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-08 14:46:23 +0300

    transactionitem component oluşturuldu. yapılan transferlerin listelenmesi için olan komponent

--- app/playground/page.js
+++ app/playground/page.js
@@ -1,4 +1,5 @@
 import PageHeader from '@/components/page-header'
+import TransactionItem from '@/components/transaction-item'
 import Trend from '@/components/trend'
 
 
@@ -24,6 +25,16 @@ const Page = () => {
                   <Trend type="Expense" amount={1700} prevAmount={1500} />
                   <Trend type="Investment" amount={300} />
                   <Trend type="Saving" amount={700} />
+              </div>
+          </div>
+          <div>
+              <h2>Transferler</h2>
+              <hr className='mb-4 border-slate-300 dark:border-slate-700' />
+              <div className='space-y-4 '>
+                  <TransactionItem type="Income" description="Maaş" amount={17000} />
+                  <TransactionItem type="Expense" category="Gıda" description="Dışarıda yemek" amount={7000}/>
+                  <TransactionItem type="Saving" category="Kişisel" description="Bilgisayar için" amount={1000}/>
+                  <TransactionItem type="Investment" description="Borsa" amount={9000} />
               </div>
           </div>
     </main>
