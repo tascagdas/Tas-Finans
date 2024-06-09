12c885b307f9d8d89510b519ff485da221deea93
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-09 09:09:38 +0300

    Transferler özeti componenti oluşturuldu (transaction-summary-item)

--- app/playground/page.js
+++ app/playground/page.js
@@ -1,5 +1,6 @@
 import PageHeader from '@/components/page-header'
 import TransactionItem from '@/components/transaction-item'
+import TransactionSummaryItem from '@/components/transaction-summary-item'
 import Trend from '@/components/trend'
 
 
@@ -34,6 +35,18 @@ const Page = () => {
                   <TransactionItem type="Income" description="Maaş" amount={17000} />
                   <TransactionItem type="Expense" category="Gıda" description="Dışarıda yemek" amount={7000}/>
                   <TransactionItem type="Saving" category="Kişisel" description="Bilgisayar için" amount={1000}/>
+                  <TransactionItem type="Investment" description="Borsa" amount={9000} />
+              </div>
+          </div>
+          <div>
+              <h2>Transfer toplam</h2>
+              <hr className='mb-4 border-slate-300 dark:border-slate-700' />
+              <div className='space-y-4 '>
+                  <TransactionSummaryItem date="27/7/2024" amount={3500} />
+                  <hr className='mb-4 border-slate-300 dark:border-slate-700' />
+                  <TransactionItem type="Income" description="Maaş" amount={17000} />
+                  <TransactionItem type="Expense" category="Gıda" description="Dışarıda yemek" amount={7000} />
+                  <TransactionItem type="Saving" category="Kişisel" description="Bilgisayar için" amount={1000} />
                   <TransactionItem type="Investment" description="Borsa" amount={9000} />
               </div>
           </div>
