40a414f6a8cf1c91d949154748c3d1592bf5208e
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-10 11:50:54 +0300

    dark mode eklendi (cookie ile blog projesindekinden kopyalandı)

--- components/page-header.js
+++ components/page-header.js
@@ -1,14 +1,18 @@
 import Link from 'next/link'
+import DarkModeToggle from './dark-mode-toggle'
+import useServerDarkMode from '@/hooks/use-server-dark-mode'
 
 
-const PageHeader = ({className}) => {
+const PageHeader = ({ className }) => {
+  const theme = useServerDarkMode();
+
   return (
-      <header className={`flex justify-between items-center ${className}`} >
-          <Link href={"/dashboard"} className='text-xl hover:underline underline-offset-8 decoration-2'>Tas Finans</Link>
-          <div className='flex items-center space-x-4'>
-              <div>Karanlık Mod</div>
-              <div>Kullanıcı bilgileri</div>
-          </div>
+    <header className={`flex justify-between items-center ${className}`} >
+      <Link href={"/dashboard"} className='text-xl hover:underline underline-offset-8 decoration-2'>Tas Finans</Link>
+      <div className='flex items-center space-x-4'>
+        <DarkModeToggle defaultMode={theme} />
+        <div>Kullanıcı bilgileri</div>
+      </div>
     </header>
   )
 }
