749ecfe94a2c7e72ca93bb9b4cf54c97b91063e9
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-11 09:51:22 +0300

    suspense ve fallback ile yükleme animasyonu tamamlandı. özel componente sahip. json server'in versiyonu düşürüldü. delay fonsiyonu yeni versiyonlarda olmadığı için.

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,11 +1,14 @@
+import { Suspense } from "react"
 import TransactionList from "./components/transaction-list"
+import TransactionListFallback from "./components/transaction-list-fallback"
 
 const Page = () => {
   return (
-      <>
-
-      <TransactionList/>
-      </>
+    <>
+      <Suspense fallback={<TransactionListFallback/>}>
+        <TransactionList />
+      </Suspense>
+    </>
   )
 }
 
