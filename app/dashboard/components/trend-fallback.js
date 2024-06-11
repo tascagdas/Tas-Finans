20b2e300aac6645b7180b08cc6619662c91f01b6
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-11 12:50:52 +0300

    Trend componenti için fallback skeleton componenti yapıldı

---
+++ app/dashboard/components/trend-fallback.js
@@ -0,0 +1,22 @@
+import Skeleton from '@/components/skeleton'
+
+const TrendFallback = () => {
+  return (
+      <>
+          <div className='space-y-5 w-3/5 lg:w-5/6'>
+              <div>
+                  <Skeleton/>
+              </div>
+              <div className='mb-2'>
+                  <Skeleton />
+              </div>
+              <div className='flex space-x-2 '>
+                  <Skeleton />
+                  <Skeleton />
+              </div>
+          </div>
+    </>
+  )
+}
+
+export default TrendFallback
