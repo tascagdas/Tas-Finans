851b0fbe30c9c5ed37b2c9bc005df0283678a589
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-14 18:50:10 +0300

    Zod ile validasyon. Videodan farklı map işlemi bulunuyor. tip ve kategoriye displayname eklendiği için zod hata veriyordu.

--- app/dashboard/components/transaction-form.js
+++ app/dashboard/components/transaction-form.js
@@ -5,16 +5,19 @@ import Input from "@/components/input"
 import Label from "@/components/label"
 import Select from "@/components/select"
 import { categories, types } from "@/lib/consts"
+import { transactionSchema } from "@/lib/validation"
 import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
 
 
 const TransactionForm = () => {
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm({
-        mode:"onTouched"
+        mode: "onTouched",
+        resolver: zodResolver(transactionSchema)
     });
 
-    const onSubmit= (data)=> console.log(data)
+    const onSubmit = (data)=> console.log(data)
 
 
   return (
@@ -23,18 +26,18 @@ const TransactionForm = () => {
               <div>
                   <Label className="mb-1">Tip</Label>
                   <Select {...register("type")}>
-                      {types.map(type => <option key={type.value}>{ type.displayName }</option>)}
+                      {types.map(type => <option key={type.value} value={type.value}>{ type.displayName }</option>)}
                   </Select>
               </div>
               <div>
                   <Label className="mb-1">Kategori</Label>
                   <Select {...register("category")}>
-                      {categories.map(category => <option key={category.value}>{category.displayName}</option>)}
+                      {categories.map(category => <option key={category.value} value={category.value}>{category.displayName}</option>)}
                   </Select>
               </div>
               <div>
                   <Label className="mb-1">İşlem Tarihi</Label>
-                  <Input {...register("transactionDate")} /> 
+                  <Input placeholder="GG-AA-YYYY" {...register("transactionDate")} /> 
                   {errors.transactionDate && <p className="mt-1 text-red-500">{errors.transactionDate.message}</p>}
               </div>
               <div>
@@ -44,8 +47,8 @@ const TransactionForm = () => {
               </div>
               <div className="col-span-1 md:col-span-2">
                   <Label className="mb-1">İşlem Açıklaması</Label>
-                  <Input type="text" {...register("transactionDescription")} />
-                  {errors.transactionDescription && <p className="mt-1 text-red-500">{errors.transactionDescription.message}</p>}
+                  <Input type="text" {...register("description")} />
+                  {errors.description && <p className="mt-1 text-red-500">{errors.description.message}</p>}
               </div>
           </div>
           <div className="flex justify-end">
