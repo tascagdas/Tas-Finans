737667be0115006a3bc6ed4d9754e260347ac41d
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-23 10:59:43 +0300

    supabase'de yazılı olan fonksiyondan veri fetchleniyor.

--- app/dashboard/components/trend.js
+++ app/dashboard/components/trend.js
@@ -1,9 +1,20 @@
 import BaseTrend from "@/components/trend"
+import { createClient } from "@/lib/supabase/server"
 const Trend = async ({ type, displayName }) => {
-    const response = await fetch(`${process.env.API_URL}/trends/${type}`);
-    const { amount, prevAmount } = await response.json();
+    
+    const supabase = createClient()
+
+    let { data, error } = await supabase
+        .rpc('calculate_total', {
+            type_arg: type
+        })
+    if (error) throw new Error('Trend verisi alınamadı.')
+
+    const amount = data ?? 0
+
+
     return (
-        <BaseTrend amount={amount} type={type} prevAmount={prevAmount} displayName={displayName} />
+        <BaseTrend amount={amount} type={type} prevAmount={amount-1000} displayName={displayName} />
     )
 }
 
