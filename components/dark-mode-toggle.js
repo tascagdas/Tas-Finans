0732657e5a7a5c21750131415f5e955f875b582c
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-27 14:56:55 +0300

    login olundugunda dashboard sayfasinda user bilgilerinin gozukmesi islemi .

--- components/dark-mode-toggle.js
+++ components/dark-mode-toggle.js
@@ -8,8 +8,8 @@ const DarkModeToggle = ({ defaultMode = 'dark' }) => {
     const { theme, toggleTheme } = useDarkMode(defaultMode)
     return (
         <Button variant='ghost' size='sm' onClick={toggleTheme}>
-            {theme == 'light' && <Moon className="w-4 h-4" />}
-            {theme == 'dark' && <Sun className="w-4 h-4" />}
+            {theme == 'light' && <Moon className="w-6 h-6" />}
+            {theme == 'dark' && <Sun className="w-6 h-6" />}
         </Button>
     )
 }
--- components/page-header.js
+++ components/page-header.js
@@ -1,17 +1,28 @@
 import Link from 'next/link'
 import DarkModeToggle from './dark-mode-toggle'
 import useServerDarkMode from '@/hooks/use-server-dark-mode'
+import { createClient } from '@/lib/supabase/server';
+import Button from './button';
+import { KeyRound, UserCircle } from 'lucide-react';
+import { sizes, variants } from '@/lib/variants';
 
 
-const PageHeader = ({ className }) => {
+const PageHeader = async ({ className }) => {
   const theme = useServerDarkMode();
-
+  const supabase = createClient();
+  const { data: { user }, error } = await supabase.auth.getUser();
   return (
     <header className={`flex justify-between items-center ${className}`} >
       <Link href={"/dashboard"} className='text-xl hover:underline underline-offset-8 decoration-2'>Tas Finans</Link>
       <div className='flex items-center space-x-4'>
         <DarkModeToggle defaultMode={theme} />
-        <div>Kullanıcı bilgileri</div>
+        {user && <Button variant='ghost' size="sm" className="flex items-center space-x-1">
+          <UserCircle/>
+          <p>{user.email}</p> 
+        </Button>}
+        {!user && <Link href="/login" className={`${variants['ghost']} ${sizes["sm"]}`}>
+        <KeyRound/>
+        </Link>}
       </div>
     </header>
   )
