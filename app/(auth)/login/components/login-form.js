838615fc26ea54eb43b7f8d6148b2b2a4cdeae84
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-27 10:42:25 +0300

    login error ve state icin ufak calisma

--- app/(auth)/login/components/login-form.js
+++ app/(auth)/login/components/login-form.js
@@ -1,12 +1,23 @@
+'use client'
 import Input from "@/components/input";
 import SubmitButton from "@/components/submit-button";
 import { login } from "@/lib/actions";
+import { useFormState } from "react-dom";
+
+const initialState = {
+    message: '',
+    error: false
+}
 
 export default function LoginForm() {
+    const [state, formAction] = useFormState(login, initialState)
     return (
-        <form action={login} className="space-y-3 flex flex-col items-center">
-                <Input type="email" placeholder="örnek@mail.com" name="email" required />
-                <SubmitButton type="submit" size="sm" className="w-60">Email ile oturum aç</SubmitButton>
+        <form action={formAction} className="space-y-3 flex flex-col items-center">
+            <Input type="email" placeholder="örnek@email.com" name="email" required />
+            <SubmitButton type="submit" size="sm" className="w-60">Email ile oturum aç</SubmitButton>
+            <p className={`${state?.error ? 'text-red-500' : 'text-green-500'}`}>
+                {state?.message}
+            </p>
         </form>
     )
 }
