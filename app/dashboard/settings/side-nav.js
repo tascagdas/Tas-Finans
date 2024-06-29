32d26fc02c7ba24f251871a3797978519410738b
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-29 16:28:55 +0300

    diger sayfalar ve linkler duzenlendi

--- app/dashboard/settings/side-nav.js
+++ app/dashboard/settings/side-nav.js
@@ -11,13 +11,13 @@ export default function SideNav() {
         <nav>
             <ul>
                 <li>
-                    <Link href="/dashboard/setting" className={`px-2.5 py-2 flex items-center space-x-2 text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 ${pathname == '/dashboard/settings'?'bg-yellow-100 dark:bg-orange-500' : '' }`}> <Settings className="w-4 h-4"/> <span>Ayarlar</span></Link>
+                    <Link href="/dashboard/settings" className={`px-2.5 py-2 flex items-center space-x-2 text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 ${pathname == '/dashboard/settings'?'bg-yellow-100 dark:bg-orange-500' : '' }`}> <Settings className="w-4 h-4"/> <span>Ayarlar</span></Link>
                 </li>
                 <li>
-                    <Link href="/dashboard/setting/avatar" className={`px-2.5 py-2 flex items-center space-x-2 text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 ${pathname == '/dashboard/settings/avatar' ? 'bg-yellow-100 dark:bg-orange-500' : ''}`}> <Camera className="w-4 h-4" /> <span>Avatar</span></Link>
+                    <Link href="/dashboard/settings/avatar" className={`px-2.5 py-2 flex items-center space-x-2 text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 ${pathname == '/dashboard/settings/avatar' ? 'bg-yellow-100 dark:bg-orange-500' : ''}`}> <Camera className="w-4 h-4" /> <span>Profil Fotoğrafı</span></Link>
                 </li>
                 <li>
-                    <Link href="/dashboard/setting/profile" className={`px-2.5 py-2 flex items-center space-x-2 text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 ${pathname == '/dashboard/settings/profile' ? 'bg-yellow-100 dark:bg-orange-500' : ''}`}> <User2 className="w-4 h-4" /> <span>Profil</span></Link>
+                    <Link href="/dashboard/settings/profile" className={`px-2.5 py-2 flex items-center space-x-2 text-sm rounded-md hover:bg-gray-100 dark:hover:bg-gray-900 ${pathname == '/dashboard/settings/profile' ? 'bg-yellow-100 dark:bg-orange-500' : ''}`}> <User2 className="w-4 h-4" /> <span>Profil</span></Link>
                 </li>
         </ul>
         </nav>
--- components/page-header.js
+++ components/page-header.js
@@ -2,7 +2,6 @@ import Link from 'next/link'
 import DarkModeToggle from './dark-mode-toggle'
 import useServerDarkMode from '@/hooks/use-server-dark-mode'
 import { createClient } from '@/lib/supabase/server';
-import Button from './button';
 import { KeyRound, UserCircle } from 'lucide-react';
 import { sizes, variants } from '@/lib/variants';
 import SignOutButton from './sign-out-button';
@@ -17,10 +16,10 @@ const PageHeader = async ({ className }) => {
       <Link href={"/dashboard"} className='text-xl hover:underline underline-offset-8 decoration-2'>Tas Finans</Link>
       <div className='flex items-center space-x-4'>
         <DarkModeToggle defaultMode={theme} />
-        {user && <Button variant='ghost' size="sm" className="flex items-center space-x-1">
+        {user && <Link href='/dashboard/settings' className={`flex items-center space-x-1 ${variants['ghost']} ${sizes['sm']}`}>
           <UserCircle/>
           <p>{user.email}</p> 
-        </Button>}
+        </Link>}
         {user && <SignOutButton/>}
         {!user && <Link href="/login" className={`${variants['ghost']} ${sizes["sm"]}`}>
         <KeyRound/>
