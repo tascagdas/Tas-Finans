e380821cd8fdb7c8876e117e205923c8c382eabc
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-30 12:51:57 +0300

    resim upload islemi ok. test edildi.

--- app/dashboard/settings/avatar/page.js
+++ app/dashboard/settings/avatar/page.js
@@ -1,11 +1,12 @@
 import Input from "@/components/input";
 import SubmitButton from "@/components/submit-button";
+import { uploadAvatar } from "@/lib/actions";
 
 export default function Page() {
     return (
         <>
             <h1 className="text-4xl font-semibold mb-8">Profil Resmi</h1>
-            <form className="space-y-4">
+            <form className="space-y-4" action={uploadAvatar}>
                 <Input type='file' name='file' id='file'/>
                 <SubmitButton>Resmi Gönder</SubmitButton>
             </form>
