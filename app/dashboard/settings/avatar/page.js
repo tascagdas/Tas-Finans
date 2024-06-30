e67176c58fe15d7063658841e4cfa008b4489d15
Çağdaş Taş <143749465+tascagdas@users.noreply.github.com>
2024-06-30 09:47:44 +0300

    resim upload formu olusturuldu

--- app/dashboard/settings/avatar/page.js
+++ app/dashboard/settings/avatar/page.js
@@ -1,7 +1,14 @@
+import Input from "@/components/input";
+import SubmitButton from "@/components/submit-button";
+
 export default function Page() {
     return (
         <>
-            Profil resmi
+            <h1 className="text-4xl font-semibold mb-8">Profil Resmi</h1>
+            <form className="space-y-4">
+                <Input type='file' name='file' id='file'/>
+                <SubmitButton>Resmi Gönder</SubmitButton>
+            </form>
         </>
     )
 }
